1af705a918cc0455f59117fdd91806d9b0bda691
ui-narayan <ui.narayanbhardwaj@gmail.com>
2021-04-10 14:17:18 +0530

    header fixed

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll > 300) {
+        //clearHeader, not clearheader - caps H
+        $(".header").addClass("header__fixed");
+    }
+    else {
+        $(".header").removeClass("header__fixed");
+    }
+}); //missing );
