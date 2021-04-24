a3bedc0b8596124d4f9e3186e11eb90915ed0488
ui-narayan <ui.narayanbhardwaj@gmail.com>
2021-04-24 21:19:26 +0530

    about text

--- index.php
+++ index.php
@@ -75,8 +75,8 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h2 class="main-title mb-5 left">About GSISD</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <h2 class="main-title mb-3 left">About GSISD</h2>
+                <p>GSISD managed SD-WAN helps simplify your WAN environment by establishing a consolidated plane across differing networks. We provide a centrally hosted solution to remotely program edge devices and make changes in real-time. With Network-as-a-Service, you can be confident of maximum performance at zero vendor lock-in.</p>
                 <a href="solutions.php" class="btn btn-primary rounded-pill">Learn More</a>
             </div>
             <div class="col text-center">
